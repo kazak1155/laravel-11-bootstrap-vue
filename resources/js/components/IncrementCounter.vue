d186db77b8b926848082eae524ef48865cbfb063
Andrey Volynkin <kazakmt4@gmail.com>
2024-04-26 10:04:02 +0300

    first commit - laravel 11 + bootstrap + vue

---
+++ resources/js/components/IncrementCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+</script>
+
+<template>
+    <button
+        type="button"
+        @click="counter++"
+    >
+        <h1>Counter</h1>
+        Counter is: {{ counter }}
+    </button>
+</template>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body>
+<body id="app" class="font-sans antialiased">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
